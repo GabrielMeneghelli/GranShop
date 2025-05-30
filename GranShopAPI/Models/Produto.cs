6489500c3346e40d3986b6456c5f3630ea0a3aa7
blessedMene <meneghelli.w7zin@gmail.com>
2025-05-30 11:55:20 -0300

    Inicio do projeto, criação da pasta models e das classes e criação do appdbcontext proximo passo: alterar o program.cs

---
+++ GranShopAPI/Models/Produto.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GranShopAPI.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Categoria")]
+        public int CategoriaID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [StringLength(300)]
+        public string Descricao { get; set; }
+
+        [Required]
+        public int Estoque { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal ValorCusto { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal ValorVenda { get; set; }
+
+        [Required]
+        public bool Destaque { get; set; }
+    }
+}
